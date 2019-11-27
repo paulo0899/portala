db05b303ef59045e5f1a8728051f5408a8236ae1
Paulo Ricardo <paulo0899@gmail.com>
2019-11-27 00:45:22 -0300

    cm

--- app/src/main/java/com/example/portala/activity/DisciplinaInstituicaoActivity.java
+++ app/src/main/java/com/example/portala/activity/DisciplinaInstituicaoActivity.java
@@ -1,12 +1,10 @@
 package com.example.portala.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.portala.R;
 
 public class DisciplinaInstituicaoActivity extends AppCompatActivity {
